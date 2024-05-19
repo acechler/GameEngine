fa25496c3001ea6ec422a5c0ef33cdaa871f28a7
Aaron Echler <acechler@gmail.com>
2024-05-19 14:38:24 -0400

    Add name property to the actor.

--- GameEngine/Actor.h
+++ GameEngine/Actor.h
@@ -1,4 +1,31 @@
-#pragma once
+#ifndef ACTOR_H
+#define ACTOR_H
+
 #include<iostream>
 #include<string>
 
+
+
+
+class Actor
+{
+public:
+	Actor();
+	~Actor();
+
+private:
+	
+	std::string name;
+
+};
+
+Actor::Actor()
+{
+}
+
+Actor::~Actor()
+{
+}
+
+
+#endif // !ACTOR_H
