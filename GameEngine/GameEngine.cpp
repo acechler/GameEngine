4b3aad6ff1f3615b558319ed9a72ddcc3fba2458
Aaron Echler <acechler@gmail.com>
2024-05-19 15:08:14 -0400

    Add test of sprite object

--- GameEngine/GameEngine.cpp
+++ GameEngine/GameEngine.cpp
@@ -2,9 +2,18 @@
 //
 
 #include <iostream>
+#include "Sprite.h"
 
 int main()
 {
     std::cout << "Hello World!\n";
+    
+    Sprite sprite(2, 2);
+    sprite.setPixel(0, 0, '#');
+    sprite.setPixel(1, 0, '#');
+    sprite.setPixel(0, 1, '#');
+    sprite.setPixel(1, 1, '#');
+
+    sprite.draw();
 }
 
